29c1bf8d3288db46a03f6f70f8c1c6331d65cec4
Arsenije Savic <arsenijesavic@me.com>
2019-08-15 09:15:56 +0200

    ...

--- src/pages/chat/sketch.js
+++ src/pages/chat/sketch.js
@@ -1,23 +1,24 @@
-export default function sketch(p) {
-  let rotation = 0
+export default () => true
+// export default function sketch(p) {
+//   let rotation = 0
 
-  p.setup = function() {
-    p.createCanvas(600, 400, p.WEBGL)
-  }
+//   p.setup = function() {
+//     p.createCanvas(600, 400, p.WEBGL)
+//   }
 
-  p.myCustomRedrawAccordingToNewPropsHandler = function(props) {
-    if (props.rotation !== null) {
-      rotation = (props.rotation * Math.PI) / 180
-    }
-  }
+//   p.myCustomRedrawAccordingToNewPropsHandler = function(props) {
+//     if (props.rotation !== null) {
+//       rotation = (props.rotation * Math.PI) / 180
+//     }
+//   }
 
-  p.draw = function() {
-    p.background(100)
-    p.normalMaterial()
-    p.noStroke()
-    p.push()
-    p.rotateY(rotation)
-    p.box(100)
-    p.pop()
-  }
-}
+//   p.draw = function() {
+//     p.background(100)
+//     p.normalMaterial()
+//     p.noStroke()
+//     p.push()
+//     p.rotateY(rotation)
+//     p.box(100)
+//     p.pop()
+//   }
+// }
