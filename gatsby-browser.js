bb37cbd7019c5057a632affe293ccf4d29e843a9
Arsenije Savic <arsenijesavic@me.com>
2019-09-13 20:34:56 +0200

    deps updates

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,7 +3,6 @@ import { ThemeProvider } from "styled-components"
 import theme from "./src/styles/theme"
 import "./src/styles/hamburgers.css"
 // import ReactGA from "react-ga"
-
 //ReactGA.initialize("UA-142414970-1")
 
 export const wrapRootElement = ({ element }) => (
